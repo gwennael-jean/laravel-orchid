ea182423f87133a3c2a5a53f02786134c8c81f91
Gwennael Jean <gwennael.jean@gmail.com>
2023-04-04 20:24:08 +0200

    feat(app): préparation du model Post

---
+++ database/migrations/2023_04_04_181922_create_table_posts.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('body');
+            $table->boolean('enabled')->default(1);
+            $table->boolean('draft')->default(0);
+            $table->boolean('sticky')->default(0);
+            $table->dateTime('published_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
